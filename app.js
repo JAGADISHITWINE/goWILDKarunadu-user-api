4fcbca708e2261edc9a37256f8466ca8a34f9184
Jagadish S <jagadishitwinetech@gmail.com>
2026-03-07 10:35:15 +0530

    update

--- app.js
+++ app.js
@@ -3,11 +3,20 @@ const cors = require("cors");
 
 const app = express();
 
-app.use(cors());
+app.use(
+  cors({
+    origin: [
+      "https://gowildkarunadu.vercel.app"
+    ],
+    methods: ["GET","POST","PUT","DELETE"],
+    credentials: true
+  })
+);
+
 app.use(express.json());
 
 app.get("/", (req, res) => {
-  res.send("API running");
+  res.send("API running 🚀");
 });
 
 module.exports = app;
